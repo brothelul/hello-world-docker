f6416827caf95fdd06054f8d010230e7a762251b
chenglu <1285823170@qq.com>
2019-11-26 22:20:11 +0800

    dockerfile修改：上传镜像到腾讯云

--- start-code.sh
+++ start-code.sh
@@ -17,6 +17,12 @@ docker run --rm \
 mv $proj_home/target/demo-*.jar $proj_home/target/demo.jar;
 docker build -t $img_output .;
 
+img_id= docker images $img_output -q
+# 上传镜像到腾讯云
+sudo docker login --username=100012207427 ccr.ccs.tencentyun.com
+sudo docker tag $img_id ccr.ccs.tencentyun.com/chenglu_test/test:$image_name
+sudo docker push ccr.ccs.tencentyun.com/chenglu_test/test:$image_name
+
 # 删除容器
 docker rm -f $img_name &> /dev/null;
 
