22e1226c081eaf6c8b4e6d7e5fe8c5abf6b24fd0
chenglu <1285823170@qq.com>
2019-11-26 23:10:59 +0800

    dockerfile修改：上传镜像到腾讯云

--- start-code.sh
+++ start-code.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-
+img_tag=$0;
+if [ ! $img_tag ]; then
+    echo "docker image tag cannot be null";
+    exit 0;
+fi
 img_mvn="maven:3.3.3-jdk-8";                 # docker image of maven
 m2_cache=~/.m2;                              # the local maven cache dir
 proj_home=$PWD;                              # the project root dir
-img_output="chenglu/demo";         # output image tag
+img_output="chenglu/demo:"+img_tag;         # output image tag
 img_name="demo";
 
 git pull  # should use git clone https://name:pwd@xxx.git
@@ -21,8 +25,8 @@ img_id= docker images $img_output -q
 # 上传镜像到腾讯云
 echo "login tencent registory"
 sudo docker login --username=100012207427 ccr.ccs.tencentyun.com
-sudo docker tag $img_output:$img_id ccr.ccs.tencentyun.com/chenglu_test/test:v1
-sudo docker push ccr.ccs.tencentyun.com/chenglu_test/test:v1
+sudo docker tag $img_output ccr.ccs.tencentyun.com/chenglu_test/test:$img_tag
+sudo docker push ccr.ccs.tencentyun.com/chenglu_test/test:$img_tag
 
 # 删除容器
 docker rm -f $img_name &> /dev/null;
