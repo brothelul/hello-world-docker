d2f3854f8d8ec9d1fd9f38385317cf8113a2bbcd
chenglu <chenglu@deepexi.com>
2019-11-26 18:23:46 +0800

    启动脚本提交

--- start-code.sh
+++ start-code.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-img_mvn="maven:3.3.3-jdk-8"                 # docker image of maven
-m2_cache=~/.m2                              # the local maven cache dir
-proj_home=$PWD                              # the project root dir
-img_output="chenglu/demo"         # output image tag
+img_mvn="maven:3.3.3-jdk-8";                 # docker image of maven
+m2_cache=~/.m2;                              # the local maven cache dir
+proj_home=$PWD;                              # the project root dir
+img_output="chenglu/demo";         # output image tag
+img_name = "demo";
 
 git pull  # should use git clone https://name:pwd@xxx.git
 
-echo "使用docker的maven"
+echo "using docker maven to build the project";
 docker run --rm \
    -v $m2_cache:/root/.m2 \
    -v $proj_home:/opt/maven \
-   -w /opt/maven $img_mvn mvn clean package -U
+   -w /opt/maven $img_mvn mvn clean package -U;
 
 mv $proj_home/target/demo-*.jar $proj_home/target/demo.jar;
-docker build -t "chenglu/demo" .;
+docker build -t $img_output .;
 
 # 删除容器
-docker rm -f demo &> /dev/null;
+docker rm -f $img_name &> /dev/null;
 
 version=`date "+%Y%m%d%H"`;
 
@@ -26,7 +27,7 @@ docker run -d --restart=on-failure:5 --privileged=true \
     --net=host \
     -w /home \
     -v $PWD/logs:/home/logs \
-    --name demo chenglu/demo \
+    --name $img_name $img_output \
     java \
         -Djava.security.egd=file:/dev/./urandom \
         -Duser.timezone=Asia/Shanghai \
@@ -36,6 +37,6 @@ docker run -d --restart=on-failure:5 --privileged=true \
         -XX:+HeapDumpOnOutOfMemoryError \
         -Xloggc:logs/gc_$version.log \
         -jar /home/demo.jar \;
-echo "删除没有名字的旧镜像";
+echo "delete the unused images";
 docker rmi $(docker images -f "dangling=true" -q)
-docker logs -f demo
+docker logs -f $img_name
